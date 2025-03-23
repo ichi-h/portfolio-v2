47eb411c73c73ed926519730e9f6ecb9a9bbb31c
ichi-h <85932406+ichi-h@users.noreply.github.com>
2025-03-23 15:04:26 +0900

    fix: comment-out meta information

--- node/packages/works-v2/src/components/pages/me/route.tsx
+++ node/packages/works-v2/src/components/pages/me/route.tsx
@@ -1,69 +1,68 @@
-import { type MetaFunction } from "@remix-run/cloudflare";
 import { Avatar, GitHubIcon, Icon, Link, Text, XIcon } from "portfolio-ui";
 
-import me from "@/assets/me_512x512.webp";
-import { Hr } from "@/components/hr";
-import { Layout } from "@/components/layout";
-import { Title } from "@/components/title";
-import { useEnv } from "@/utils/env";
+import me from "../../../assets/me_512x512.webp?url";
+// import { useEnv } from "../../../utils/env";
+import { Hr } from "../../parts/hr";
+import { Layout } from "../../parts/layout";
+import { Title } from "../../parts/title";
 
 import * as styles from "./route.css";
 
-export const meta: MetaFunction = () => [
-  { title: "Me - ichi-h.com", charSet: "utf-8" },
-  {
-    name: "viewport",
-    content: "width=device-width,initial-scale=1",
-  },
-  {
-    name: "robots",
-    content: "index, follow",
-  },
-  {
-    property: "og:url",
-    content: "https://ichi-h.com/me",
-  },
-  {
-    property: "og:type",
-    content: "article",
-  },
-  {
-    property: "og:title",
-    content: "Me - ichi-h.com",
-  },
-  {
-    property: "og:image",
-    content: `${useEnv().APP_URL}/top_ogp.webp`,
-  },
-  {
-    property: "og:site_name",
-    content: "ichi-h.com",
-  },
-  {
-    property: "og:description",
-    content: "To live is to think and create.",
-  },
-  {
-    name: "twitter:title",
-    content: "Me - ichi-h.com",
-  },
-  {
-    name: "twitter:card",
-    content: "summary_large_image",
-  },
-  {
-    name: "twitter:description",
-    content: "To live is to think and create.",
-  },
-  {
-    name: "twitter:domain",
-    content: "ichi-h.com",
-  },
-  {
-    name: "twitter:site",
-    content: "@ichi_h3",
-  },
-];
+// export const meta: MetaFunction = () => [
+//   { title: "Me - ichi-h.com", charSet: "utf-8" },
+//   {
+//     name: "viewport",
+//     content: "width=device-width,initial-scale=1",
+//   },
+//   {
+//     name: "robots",
+//     content: "index, follow",
+//   },
+//   {
+//     property: "og:url",
+//     content: "https://ichi-h.com/me",
+//   },
+//   {
+//     property: "og:type",
+//     content: "article",
+//   },
+//   {
+//     property: "og:title",
+//     content: "Me - ichi-h.com",
+//   },
+//   {
+//     property: "og:image",
+//     content: `${useEnv().APP_URL}/top_ogp.webp`,
+//   },
+//   {
+//     property: "og:site_name",
+//     content: "ichi-h.com",
+//   },
+//   {
+//     property: "og:description",
+//     content: "To live is to think and create.",
+//   },
+//   {
+//     name: "twitter:title",
+//     content: "Me - ichi-h.com",
+//   },
+//   {
+//     name: "twitter:card",
+//     content: "summary_large_image",
+//   },
+//   {
+//     name: "twitter:description",
+//     content: "To live is to think and create.",
+//   },
+//   {
+//     name: "twitter:domain",
+//     content: "ichi-h.com",
+//   },
+//   {
+//     name: "twitter:site",
+//     content: "@ichi_h3",
+//   },
+// ];
 
 export default function Index() {
   return (

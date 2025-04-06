c9b451ec71c2cf6ca3b7fd12543fc3f4cbf678a6
ichi-h <85932406+ichi-h@users.noreply.github.com>
2025-04-06 14:32:22 +0900

    feat: move contact page from v1

---
+++ node/packages/works-v2/src/components/pages/contact/index.tsx
@@ -0,0 +1,14 @@
+import { Text } from "portfolio-ui";
+
+import { Title } from "../../parts/title";
+
+export const Contact = () => {
+  return (
+    <>
+      <Title>Contact</Title>
+      <Text fontSize="4" color="mono.900">
+        Coming soon...
+      </Text>
+    </>
+  );
+};

bba0238773bd98b1fce307d8ffcff10d039f4c7e
ichi-h <85932406+ichi-h@users.noreply.github.com>
2025-04-29 12:29:42 +0900

    feat(env): create .env file and update environment variable access

--- node/packages/works/src/utils/env.ts
+++ node/packages/works/src/utils/env.ts
@@ -7,11 +7,11 @@ type Keys =
 
 export const useEnv = () => {
   const env = {
-    ENVIRONMENT: import.meta.env.VITE_ENVIRONMENT,
-    APP_URL: import.meta.env.VITE_APP_URL,
-    OG_IMAGE_SERVER_URL: import.meta.env.VITE_OG_IMAGE_SERVER_URL,
-    NOTION_SECRET_KEY: import.meta.env.VITE_NOTION_SECRET_KEY,
-    NOTION_DATABASE_ID: import.meta.env.VITE_NOTION_DATABASE_ID,
+    ENVIRONMENT: import.meta.env.ENVIRONMENT,
+    APP_URL: import.meta.env.APP_URL,
+    OG_IMAGE_SERVER_URL: import.meta.env.OG_IMAGE_SERVER_URL,
+    NOTION_SECRET_KEY: import.meta.env.NOTION_SECRET_KEY,
+    NOTION_DATABASE_ID: import.meta.env.NOTION_DATABASE_ID,
   } as const;
 
   const missingEnv = Object.entries(env)

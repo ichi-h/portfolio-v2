1fc53a7be2925c9ba70abdfba99d46e6d4f3bd49
ichi-h <85932406+ichi-h@users.noreply.github.com>
2025-08-03 12:53:35 +0900

    feat: add links to categories page.

--- node/packages/works/src/components/pages/categories.[category]/index.css.ts
+++ node/packages/works/src/components/pages/categories.[category]/index.css.ts
@@ -1,4 +1,4 @@
 import { style } from "@vanilla-extract/css";
-import { flex, flexDirection, gap } from "portfolio-styles";
+import { flex, flexJustify } from "portfolio-styles";
 
-export const layout = style([flex, flexDirection["column"], gap[4]]);
+export const tagLinkContainer = style([flex, flexJustify["end"]]);
--- node/packages/works/src/components/pages/categories.[category]/index.tsx
+++ node/packages/works/src/components/pages/categories.[category]/index.tsx
@@ -3,10 +3,24 @@ import { GridWorks } from "../../parts/gridWorks";
 import type { Work } from "../../../api/notion/works";
 import type { FC } from "react";
 
+import * as style from "./index.css";
+
+import { Link } from "portfolio-ui";
+import { Anchor } from "../../parts/anchor";
+
 interface Props {
   works: Work[];
 }
 
 export const Category: FC<Props> = ({ works }) => {
-  return <GridWorks works={works} />;
+  return (
+    <>
+      <div className={style.tagLinkContainer}>
+        <Link as={Anchor} asProps={{ href: "/categories" }}>
+          All categories &gt;
+        </Link>
+      </div>
+      <GridWorks works={works} />
+    </>
+  );
 };

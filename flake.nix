1becbcd2d7af4e342d3110723134d39a90956eb0
ichi-h <85932406+ichi-h@users.noreply.github.com>
2026-03-02 20:10:39 +0900

    Add multi-agent-shogun

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "portfolio-v2";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        packages = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells = {
+          default = packages.mkShell {
+            buildInputs = with packages; [
+              inotify-tools
+            ];
+          };
+        };
+      }
+    );
+}

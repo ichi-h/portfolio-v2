9170680a71e188deac85f6a70d7cf2a946f60948
ichi-h <85932406+ichi-h@users.noreply.github.com>
2026-04-08 23:43:27 +0900

    feat: add funds.md

--- node/packages/tech/src/components/pages/home/md/resume/index.ts
+++ node/packages/tech/src/components/pages/home/md/resume/index.ts
@@ -1,5 +1,6 @@
+import funds from "./funds.md?raw";
 import wizleap from "./wizleap.md?raw";
 
 export * as summary from "./summary.md?raw";
 
-export const resume = [wizleap];
+export const resume = [funds, wizleap];

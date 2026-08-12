5ba5859c39b5e2a454467737875f1f2e655554e5
ichi-h <85932406+ichi-h@users.noreply.github.com>
2026-08-12 16:28:01 +0900

    feat: add author component.

---
+++ node/packages/works/src/components/parts/author.css.ts
@@ -0,0 +1,45 @@
+import { style } from "@vanilla-extract/css";
+import {
+  animateZoomOnHover,
+  applyMedia,
+  dropShadow,
+  flattenStyle,
+  flex,
+  flexDirection,
+  flexJustify,
+  gap,
+  h,
+  mb,
+  p,
+  textAlign,
+  w,
+} from "portfolio-styles";
+
+export const authorBox = style([
+  flex,
+  gap[4],
+  p[2],
+  applyMedia({ max: "480" }, flattenStyle([flexDirection["column"], gap[2]])),
+]);
+
+export const avatarBox = style([
+  h["fit"],
+  applyMedia({ max: "480" }, flattenStyle([textAlign["center"]])),
+]);
+
+export const profileBox = style([flex, flexDirection["column"], gap[2]]);
+
+export const profileName = style([
+  applyMedia({ max: "480" }, flattenStyle([textAlign["center"]])),
+]);
+
+export const avatar = style([
+  dropShadow["soft"],
+  w[24],
+  h[24],
+  animateZoomOnHover["md"],
+]);
+
+export const snsLinks = style([
+  applyMedia({ max: "480" }, flattenStyle([flex, flexJustify["end"]])),
+]);
---
+++ node/packages/works/src/components/parts/author.tsx
@@ -0,0 +1,27 @@
+import { Avatar, Link, Paragraph, Text } from "portfolio-ui";
+
+import me from "../../assets/me_512x512.webp?url";
+import * as styles from "./author.css";
+import { SNSLinks } from "./sns-links";
+import { PROFILE_DESCRIPTIONS } from "../../constants/profile";
+
+export const Author = () => (
+  <div className={styles.authorBox}>
+    <Link href="/about" color="mono.900" className={styles.avatarBox}>
+      <Avatar className={styles.avatar} src={me} />
+    </Link>
+    <div className={styles.profileBox}>
+      <Paragraph className={styles.profileName}>
+        <Link href="/about" color="mono.900">
+          <Text weight="bold" fontSize="6">
+            ichi-h
+          </Text>
+        </Link>
+      </Paragraph>
+      <Paragraph>{PROFILE_DESCRIPTIONS[0]}</Paragraph>
+      <Paragraph>{PROFILE_DESCRIPTIONS[1]}</Paragraph>
+      <Paragraph>{PROFILE_DESCRIPTIONS[2]}</Paragraph>
+      <SNSLinks className={styles.snsLinks} variant="dark" gap="small" />
+    </div>
+  </div>
+);
